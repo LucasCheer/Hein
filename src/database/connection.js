cb9d0c3b56d8270bea78ae449a54b5cf31414fae
Lucas <cheroyonatan2@gmail.com>
2024-09-23 18:19:01 -0300

    Conexion con la base de datos

---
+++ src/database/connection.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import sql from 'mssql';
+
+dotenv.config();
+
+const dbSettings = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    },
+    connectionTimeout: 30000, 
+};
+
+export const getConnection = async () => {
+    try {
+        const pool = await sql.connect(dbSettings);
+        console.log("Conectado a la base de datos!");
+        return pool;
+        
+    } catch (error) { //
+        if (error instanceof sql.ConnectionError) {
+            console.error("Error de conexión:", error.message);
+        } else {
+            console.error("Otro error:", error.message);
+        }
+        console.log("Error al conectarse a la base de datos");
+    }
+};
+
+getConnection();
