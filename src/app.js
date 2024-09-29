0a83a7effbe2a2496a032abf00a51fe11b222217
Lucas <cheroyonatan2@gmail.com>
2024-09-28 22:49:04 -0300

    instalo cors al proyecto

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import routesUsers from './routes/users.routes.js'
 import routesRegisters from './routes/register.routes.js'
 
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+    credentials: true
+}));
 
 app.use(express.json())
 
