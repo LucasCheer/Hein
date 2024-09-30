c0b6e72db18989d732fcd79c2b8009f55c543436
Lucas <cheroyonatan2@gmail.com>
2024-09-29 21:44:16 -0300

    boton agregar arreglado

--- public/js/diary.js
+++ public/js/diary.js
@@ -12,32 +12,32 @@ cards.forEach((card) => {
 });
 
 //Cargar el contenido de un dia en especifico 
-document.addEventListener("DOMContentLoaded", () => {
-    const textArea = document.getElementById('textarea');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const textArea = document.getElementById('textarea');
     
-    fetch("http://localhost:3000/registros/8")
-    .then(response => response.json())
-    .then(data => {
-        // Asigna la descripción al textarea
-        textArea.value = data.Descripcion;
+//     fetch("http://localhost:3000/registros")
+//     .then(response => response.json())
+//     .then(data => {
+//         // Asigna la descripción al textarea
+//         textArea.value = data.Descripcion;
 
-        // Obtén el valor del IdEmocion y conviértelo a string
-        const IdEmocionValue = data.IdEmocion.toString();
-        console.log(IdEmocionValue)
+//         // Obtén el valor del IdEmocion y conviértelo a string
+//         const IdEmocionValue = data.IdEmocion.toString();
+//         console.log(IdEmocionValue)
 
-        // Compara el valor de IdEmocion con el value de cada tarjeta
-        cards.forEach(card => {
-            const cardValue = card.getAttribute('data-value'); // Obtén el valor de 'data-value'
-            console.log(cardValue); // Debería mostrar el valor correcto
-            if (cardValue === IdEmocionValue) {
-                card.classList.add('selected'); // Añade la clase 'selected' si coinciden
-            }
-        });
-    })
-    .catch(error => {
-        console.error(error);
-    });
-});
+//         // Compara el valor de IdEmocion con el value de cada tarjeta
+//         cards.forEach(card => {
+//             const cardValue = card.getAttribute('data-value'); // Obtén el valor de 'data-value'
+//             console.log(cardValue); // Debería mostrar el valor correcto
+//             if (cardValue === IdEmocionValue) {
+//                 card.classList.add('selected'); // Añade la clase 'selected' si coinciden
+//             }
+//         });
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// });
 
 //Enviar un nuevo registro de emocion
 
