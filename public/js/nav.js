9511a2a4522a8e8db059ee9e8e04d90c453030aa
Lucas <cheroyonatan2@gmail.com>
2024-10-01 00:11:49 -0300

    arreglo rutas 7

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,5 @@
 const newHtmlFile = new XMLHttpRequest();
-newHtmlFile.open('GET', '../../public/html/nav.html', true);
+newHtmlFile.open('GET', '../html/nav.html', true);
 newHtmlFile.send();
 newHtmlFile.onload = function () {
     document.getElementById('CargarNav').innerHTML = newHtmlFile.responseText;
--- public/js/nav2.js
+++ public/js/nav2.js
@@ -1,5 +1,5 @@
 const newHtmlFile = new XMLHttpRequest();
-newHtmlFile.open('GET', '../../public/html/nav2.html', true);
+newHtmlFile.open('GET', '../html/nav2.html', true);
 newHtmlFile.send();
 newHtmlFile.onload = function () {
     document.getElementById('CargarNav').innerHTML = newHtmlFile.responseText;
